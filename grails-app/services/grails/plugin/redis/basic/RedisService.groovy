513b44367cbe20573ec0f52998504425442e39dc
Pablo Vilas <pablovilas89@gmail.com>
2016-01-31 18:16:32 -0300

    Replaced some methods with methodMissing call

--- grails-app/services/grails/plugin/redis/basic/RedisService.groovy
+++ grails-app/services/grails/plugin/redis/basic/RedisService.groovy
@@ -2,6 +2,7 @@ package grails.plugin.redis.basic
 
 import redis.clients.jedis.Jedis
 import redis.clients.jedis.JedisPool
+import redis.clients.jedis.Transaction
 
 import javax.annotation.PreDestroy
 
@@ -25,46 +26,27 @@ class RedisService {
         }
     }
 
+    def withTransaction(Closure closure) {
+        withRedis { Jedis redis ->
+            Transaction transaction = redis.multi()
+            try {
+                closure(transaction)
+            } catch(Exception ex) {
+                transaction.discard()
+                throw ex
+            }
+            return transaction.exec()
+        }
+    }
+
     def withRedisPool(Closure closure) {
         return closure(redisPool)
     }
 
-    def scriptLoad(String script) {
-        def sha = null
+    def methodMissing(String name, args) {
+        def value = null
         withRedis { Jedis redis ->
-            sha = redis.scriptLoad(script)
-        }
-        return sha
-    }
-
-    def scriptExists(String sha) {
-        def exists = false
-        withRedis { Jedis redis ->
-            exists = redis.scriptExists(sha)
-        }
-        return exists
-    }
-
-    def evalSha(String sha) {
-        def value = false
-        withRedis { Jedis redis ->
-            value = redis.evalsha(sha)
-        }
-        return value
-    }
-
-    def evalSha(String sha, List<String> keys, List<String> args) {
-        def value = false
-        withRedis { Jedis redis ->
-            value = redis.evalsha(sha, keys, args)
-        }
-        return value
-    }
-
-    def evalSha(String sha, int keyCount, String... params) {
-        def value = false
-        withRedis { Jedis redis ->
-            value = redis.evalsha(sha, keyCount, params)
+            value = redis.invokeMethod(name, args)
         }
         return value
     }
